e4e0b5173ff96f2a4758dd364d1171cf1a794320
assu786 <shaikaqsa.78@gmail.com>
2024-12-11 11:08:22 +0530

    Add files via upload

---
+++ nonvoid.java
@@ -0,0 +1,11 @@
+class a{
+    public static String c()
+    {
+        return "hi hello bye bye";
+    }
+    public static void main(String args[])
+    {
+        String res=c();
+        System.out.println(res);
+    }
+}
